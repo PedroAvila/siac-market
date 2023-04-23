c01780e46d732667ce57e3b72f00c448b757a9b7
Pedro Ulises Ávila Núñez <minimainor_austin@hotmail.com>
2023-04-23 03:06:06 -0500

    implementando Spring Cloud Config - Configuracion Centralizada usando GitHub

--- src/main/java/com/example/consumirapi/SiacMarketApplication.java
+++ src/main/java/com/example/consumirapi/SiacMarketApplication.java
@@ -1,25 +1,16 @@
 package com.example.consumirapi;
 
-import com.example.consumirapi.web.AzureKeyVaultInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import java.io.IOException;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SiacMarketApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(SiacMarketApplication.class, args);
-		SpringApplication app = new SpringApplication(SiacMarketApplication.class);
-		ConfigurableEnvironment env = app.run(args).getEnvironment();
-		env.getPropertySources().stream().filter(ps-> ps.containsProperty("spring.cloud.azure.keyvault.secret.endpoint")).findFirst().ifPresent(ps->{
-			AzureKeyVaultInitializer azureKeyVaultInitializer = new AzureKeyVaultInitializer(ps.getProperty("spring.cloud.azure.keyvault.secret.endpoint").toString());
-			azureKeyVaultInitializer.init();
-			env.getPropertySources().addFirst(new PropertiesPropertySource("azure", azureKeyVaultInitializer.getAzureProperties()));
-		});
-
-		app.run(args);
+		SpringApplication.run(SiacMarketApplication.class, args);
 	}
-
 }
